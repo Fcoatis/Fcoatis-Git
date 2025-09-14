1cebe788ce718334b4c6130eee2c7f3cfee4e6b3
Fcoatis <Fcoatis@gmail.com>
2025-09-14 17:29:29 -0300

    Painel via class Dashboard

---
+++ dashboard.py
@@ -0,0 +1,9 @@
+class Dashboard:
+    def __init__(self):
+        self.title = 'Welcome to Dashboard!'
+
+# Criando um objeto da classe
+app = Dashboard()
+
+# Agora você pode acessar o atributo
+print(app.title)
